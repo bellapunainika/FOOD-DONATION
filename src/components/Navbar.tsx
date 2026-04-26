f20486b1d161e600ecae1548a510636f1c603bfc
Srihaas Iruventi <srihaas24@gmail.com>
2026-04-26 19:35:20 +0530

    Resolving conflict

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
               <div className="p-2 bg-brand-100 rounded-full text-brand-600">
                 <Heart size={24} className="fill-brand-500" />
               </div>
-              <span className="font-bold text-xl text-gray-900 tracking-tight">Feed Hunger</span>
+              <span className="font-bold text-xl text-gray-900 dark:text-gray-200 tracking-tight transition-colors duration-300">Feed Hunger</span>
             </Link>
           </div>
           
